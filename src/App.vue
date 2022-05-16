a83dbfd1713efa96473d8a040f4d5da0f67e2037
Toriola Faidat <faaedah@gmail.com>
2022-05-16 18:55:21 +0100

    added images & photos to assets folder

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Jost', sans-serif;
+    }
+
+
 </style>
