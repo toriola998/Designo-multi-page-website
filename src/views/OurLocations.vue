cb369c78fbefc99b8242ab2f2054db44f9a0fd62
Toriola Faidat <faaedah@gmail.com>
2022-05-20 19:50:25 +0100

    added other locations to location page

--- src/views/OurLocations.vue
+++ src/views/OurLocations.vue
@@ -6,8 +6,24 @@
                     office="Designo Central Office"
                     address="3886 Wellington Street, Toronto, Ontario M9C 3J5"
                     number="P : +1 253-863-8967"
-                    email="M : contact@designo.co" />
-
+                    email="M : contact@designo.co" 
+                />
+        <EachLocation mapImage="image-map-australia.png"
+                    mapAltText="Image of the map to Designo's office in Australia"
+                    country="Australia"
+                    office="Designo AU Office"
+                    address="19 Balonne Street, New South Wales 2443"
+                    number="P : (02) 6720 9092"
+                    email="M : contact@designo.au" 
+                />
+        <EachLocation mapImage="image-map-united-kingdom.png"
+                    mapAltText="Image of the map to Designo's office in United Kingdom"
+                    country="United Kingdom"
+                    office="Designo UK Office"
+                    address="13 Colorado Way, Rhyd-y-fro SA8 9GA"
+                    number="P : 078 3115 1400"
+                    email="M : contact@designo.uk" 
+                />
     </div>
 </template>
 
