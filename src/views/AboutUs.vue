45fac89b8fd78827c11f1abf3bbcb503d3164a7a
Toriola Faidat <faaedah@gmail.com>
2022-05-20 20:35:35 +0100

    added contents to about

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,5 +1,17 @@
 <template>
-    
+    <div>
+        <header>
+            <img src="./../assets/about/mobile/image-about-hero.jpg" alt=""/>
+            <div class="about">
+                <h1>About Us</h1>
+                <p>Founded in 2010, we are a creative agency that produces lasting results
+                for our clients. We've partnered with many startups, corporations, and nonprofits
+                alike to craft designs that make real impact. We're always looking forward to 
+                creating brands, products, and digital experiences that connect with our 
+                clients' audiences.</p>
+            </div>
+        </header>
+    </div>
 </template>
 
 <script>
@@ -8,6 +20,27 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import './../assets/scss/partials/_bg-image.scss';
+    img {
+        width: 100%;
+        display: block;
+    }
+    
+    .about {
+       @include backgroundImage;
+       background-image: url("./../assets/home/desktop/bg-pattern-hero-home.svg");
+       padding: 4rem 1.5rem;
+
+       h1, p {
+           text-align: center;
+           color: #fff;
+       }
+
+       p {
+           line-height: 1.8;
+           padding-top: 2rem;
+       }
+    }
 
 </style>
--- src/views/OurLocations.vue
+++ src/views/OurLocations.vue
@@ -1,29 +1,31 @@
 <template>
     <div>
-        <EachLocation mapImage="image-map-canada.png"
-                    mapAltText="Image of the map to Designo's office in Canada"
-                    country="Canada"
-                    office="Designo Central Office"
-                    address="3886 Wellington Street, Toronto, Ontario M9C 3J5"
-                    number="P : +1 253-863-8967"
-                    email="M : contact@designo.co" 
-                />
-        <EachLocation mapImage="image-map-australia.png"
-                    mapAltText="Image of the map to Designo's office in Australia"
-                    country="Australia"
-                    office="Designo AU Office"
-                    address="19 Balonne Street, New South Wales 2443"
-                    number="P : (02) 6720 9092"
-                    email="M : contact@designo.au" 
-                />
-        <EachLocation mapImage="image-map-united-kingdom.png"
-                    mapAltText="Image of the map to Designo's office in United Kingdom"
-                    country="United Kingdom"
-                    office="Designo UK Office"
-                    address="13 Colorado Way, Rhyd-y-fro SA8 9GA"
-                    number="P : 078 3115 1400"
-                    email="M : contact@designo.uk" 
-                />
+        <main>
+            <EachLocation mapImage="image-map-canada.png"
+                        mapAltText="Image of the map to Designo's office in Canada"
+                        country="Canada"
+                        office="Designo Central Office"
+                        address="3886 Wellington Street, Toronto, Ontario M9C 3J5"
+                        number="P : +1 253-863-8967"
+                        email="M : contact@designo.co" 
+                    />
+            <EachLocation mapImage="image-map-australia.png"
+                        mapAltText="Image of the map to Designo's office in Australia"
+                        country="Australia"
+                        office="Designo AU Office"
+                        address="19 Balonne Street, New South Wales 2443"
+                        number="P : (02) 6720 9092"
+                        email="M : contact@designo.au" 
+                    />
+            <EachLocation mapImage="image-map-united-kingdom.png"
+                        mapAltText="Image of the map to Designo's office in United Kingdom"
+                        country="United Kingdom"
+                        office="Designo UK Office"
+                        address="13 Colorado Way, Rhyd-y-fro SA8 9GA"
+                        number="P : 078 3115 1400"
+                        email="M : contact@designo.uk" 
+                    />
+        </main>
     </div>
 </template>
 
