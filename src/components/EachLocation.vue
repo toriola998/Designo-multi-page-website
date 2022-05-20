cc32b4e19848d48d07b1ed997ce495f23082eee5
Toriola Faidat <faaedah@gmail.com>
2022-05-20 19:31:29 +0100

    woh, added plenty changes

--- src/components/EachLocation.vue
+++ src/components/EachLocation.vue
@@ -1,31 +1,39 @@
 <template>
-    <div>
-        <img src="./../assets\locations/desktop/image-map-canada.png" alt=""/>
-        <PatternBackground>
-            <h2 class="location">Canada</h2>
-                <address>
-                Designo Central Office<br>
-                <span>3886 Wellington Street,Toronto, Ontario M9C 3J5</span>
-                </address>
+    <img :src="require(`./../assets/locations/desktop/${mapImage}`)" 
+         :alt="mapAltText" 
+         class="map"
+    />
+    <div class="light-bg-pattern">
+        <h2 class="location">{{ country }}</h2>
+            <address>{{ office }}<br>
+                <span>{{ address }}</span>
+            </address>
 
-            <p class="contact">Contact Us (Central Office) <br>
-                <a href=""> P : +1 253-863-8967</a>
-                <a href=""> M : contact@designo.co </a>
-            </p>
-        </PatternBackground>
+        <p class="contact">Contact <br>
+            <a href="">{{ number }}</a>
+            <a href=""> {{ email}} </a>
+        </p>
     </div>
 </template>
 
 <script>
-import PatternBackground from './../components/PatternBackground.vue'
 export default {
-    components: { PatternBackground },
-    props: ['location', 'officeName', 'address', 'officeContact', 'number', 'email' ]
+    props: [ 'mapImage', 'mapAltText', 'country', 'office', 'address','number', 'email' ]
 }
 </script>
 
 <style lang="scss" scoped>
-     .location {
+    @import './../assets/scss/partials/_light-bg-pattern.scss';
+
+   .light-bg-pattern {
+       @include bg;
+    }
+
+    .map {
+        display: block;
+    }
+
+    .location {
         color: var(--dark-peach);
         text-align: center;
     }
--- src/views/OurLocations.vue
+++ src/views/OurLocations.vue
@@ -1,19 +1,24 @@
 <template>
     <div>
-        
+        <EachLocation mapImage="image-map-canada.png"
+                    mapAltText="Image of the map to Designo's office in Canada"
+                    country="Canada"
+                    office="Designo Central Office"
+                    address="3886 Wellington Street, Toronto, Ontario M9C 3J5"
+                    number="P : +1 253-863-8967"
+                    email="M : contact@designo.co" />
+
     </div>
 </template>
 
 <script>
 
-
+import EachLocation from './../components/EachLocation.vue'
 export default {
-  components: { PatternBackground },
     name: 'Locations',
+    components: {
+        EachLocation 
+   },
+   
 }
 </script>
-
-<style lang="scss" scoped>
-   
-
-</style>>
