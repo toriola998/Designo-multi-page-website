a3b6501335258030f2a9e1621290efbe33254787
Toriola <faaedah@gmail.com>
2023-07-05 22:55:00 +0100

    REFACTOR: moved locations to an array

--- src/views/OurLocations.vue
+++ src/views/OurLocations.vue
@@ -3,30 +3,15 @@
         <main>
             <div class="location-container">
                 <div>
-                <EachLocation mapImage="image-map-canada.png"
-                            mapAltText="Image of the map to Designo's office in Canada"
-                            country="Canada"
-                            office="Designo Central Office"
-                            address="3886 Wellington Street, Toronto, Ontario M9C 3J5"
-                            number="P : +1 253-863-8967"
-                            email="M : contact@designo.co" 
-                        />
-                <EachLocation mapImage="image-map-australia.png"
-                            mapAltText="Image of the map to Designo's office in Australia"
-                            country="Australia"
-                            office="Designo AU Office"
-                            address="19 Balonne Street, New South Wales 2443"
-                            number="P : (02) 6720 9092"
-                            email="M : contact@designo.au" 
-                        />
-                <EachLocation mapImage="image-map-united-kingdom.png"
-                            mapAltText="Image of the map to Designo's office in United Kingdom"
-                            country="United Kingdom"
-                            office="Designo UK Office"
-                            address="13 Colorado Way, Rhyd-y-fro SA8 9GA"
-                            number="P : 078 3115 1400"
-                            email="M : contact@designo.uk" 
-                        />
+                <EachLocation v-for="(item, index) in locations" :key="index"
+                    :mapImage="item.image"
+                    :mapAltText="item.alt"
+                    :country="item.country"
+                    :office="item.office"
+                    :address="item.address"
+                    :number="item.number"
+                    :email="item.email" 
+                />
                 </div>
             </div>
         </main>
@@ -37,6 +22,39 @@
 import EachLocation from './../components/EachLocation.vue'
 export default {
     name: 'Locations',
+    data() {
+        return {
+            locations: [
+                {
+                    image: 'image-map-canada.png',
+                    alt: "Image of the map to Designo's office in Canada",
+                    country: 'Canada',
+                    office: 'Designo Central Office',
+                    address: '3886 Wellington Street, Toronto, Ontario M9C 3J5',
+                    number: 'P : +1 253-863-8967',
+                    email: 'M : contact@designo.co'
+                },
+                {
+                    image: 'image-map-australia.png',
+                    alt: "Image of the map to Designo's office in Australia",
+                    country: 'Australia',
+                    office: 'Designo AU Office',
+                    address: '19 Balonne Street, New South Wales 2443',
+                    number: 'P : (02) 6720 9092',
+                    email: 'M : contact@designo.au'
+                },
+                {
+                    image: 'image-map-united-kingdom.png',
+                    alt: "Image of the map to Designo's office in United Kingdom",
+                    country: 'United Kingdom"',
+                    office: 'Designo UK Office',
+                    address: '13 Colorado Way, Rhyd-y-fro SA8 9GA',
+                    number: 'P : 078 3115 1400',
+                    email: 'M : contact@designo.uk'
+                }
+            ]
+        }
+    },
     components: {
         EachLocation 
    },
