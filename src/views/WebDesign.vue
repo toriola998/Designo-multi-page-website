0d121b2b07dfa78d659eca12d680273a35058ee7
Toriola <faaedah@gmail.com>
2023-07-06 00:31:36 +0100

    REFACTOR: moved web data to json file

--- src/views/WebDesign.vue
+++ src/views/WebDesign.vue
@@ -9,49 +9,23 @@
         <main>
         <section aria-label="some of the web designs done by Designo">
             <div class="projects-wrapper">
-                <ProjectDetails projectImage="web-design/desktop/image-express.jpg"
-                                projectName="EXPRESS" 
-                                projectInfo="A multi-carrier shipping website for ecommerce businesses" 
-                                projectAltText="Screenshot of interface of the Express website"
-                            />
-                <ProjectDetails projectImage="web-design/desktop/image-transfer.jpg"
-                                projectName="TRANSFER" 
-                                projectInfo="Site for low-cost money transfers and sending money within seconds" 
-                                projectAltText="Screenshot of interface of the Transfer website" 
-                            />
-                <ProjectDetails projectImage="web-design/desktop/image-photon.jpg"
-                                projectName="PHOTON" 
-                                projectInfo="A state-of-the-art music player with high-resolution audio and DSP effects" 
-                                projectAltText="Screenshot of interface of the Photon website" 
-                            />
-                <ProjectDetails projectImage="web-design/desktop/image-builder.jpg"
-                                projectName="BUILDER" 
-                                projectInfo="Connects users with local contractors based on their location" 
-                                projectAltText="Screenshot of interface of the Builder website" 
-                            />
-                <ProjectDetails projectImage="web-design/desktop/image-blogr.jpg"
-                                projectName="BLOGR" 
-                                projectInfo="Blogr is a platform for creating an online blog or publication" 
-                                projectAltText="Screenshot of interface of the Blogr website" 
-                            />
-                <ProjectDetails projectImage="web-design/desktop/image-camp.jpg"
-                                projectName="CAMP" 
-                                projectInfo="Get expert training in coding, data, design, and digital marketing" 
-                                projectAltText="Screenshot of interface of the Camp website" 
-                            />
+                <ProjectDetails v-for="(item, index) in filteredData" :key="index"
+                    :projectImage="item.image"
+                    :projectName="item.name" 
+                    :projectInfo="item.description" 
+                    :projectAltText="item.alt"
+                />
             </div>
         </section> 
 
         <section aria-label="Other services we render at Designo">
             <div class="our-services">
-                <div class="app-design design" 
-                    @click="this.$router.push({ path: '/app-design'})">
+                <router-link to="/app-design" class="app-design design">
                     <OurServices service="APP DESIGN" />
-                </div>
-                <div class="graphic-design design"
-                    @click="this.$router.push({ path: '/graphic-design'})">
+                </router-link>
+                <router-link to="/graphic-design" class="graphic-design design">
                     <OurServices service="GRAPHIC DESIGN" />
-                </div>
+                </router-link>
             </div>
         </section>
         </main>
@@ -59,11 +33,22 @@
 </template>
 
 <script>
+import data from './../../data.json'
 import ServicesHeader from './../components/ServicesHeader.vue';
 import ProjectDetails from '../components/ProjectDetails.vue';
 import OurServices from '../components/OurServices.vue'
 export default {
     name: 'Web Design',
+    data() {
+        return {
+            data: data
+        }
+    },
+    computed: {
+        filteredData() {
+            return this.data.filter(item => item.category === 'web');
+        }
+    },
     components: {
         ServicesHeader,
         ProjectDetails,
